805fdf793dece00555ad2471f942ec495a96ad7e
tabaobk <tathanhbaobk@gmail.com>
2022-09-20 12:33:50 +0700

    deleted unnesscess files

--- week3/android/app/src/main/kotlin/com/example/o_an_quan/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.o_an_quan
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- week3/lib/models/box.dart
+++
@@ -1,6 +0,0 @@
-class Box {
-  int score;
-  bool isMandari;
-  Box(this.score, this.isMandari);
-
-}
